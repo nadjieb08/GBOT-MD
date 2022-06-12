d889c6c19c6831a6990c99d5a7afecc8acbb0d98
nadjieb08 <97201898+nadjieb08@users.noreply.github.com>
2022-06-12 10:25:59 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
-global.packname = 'Bot Whatsapp'
-global.author = 'TRASH'
-global.sessionName = 'data'
+global.owner = ['6282234343854','62895622837076']
+global.packname = 'GBOT-MD'
+global.author = 'Kevin'
+global.sessionName = 'session'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'Kevin'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
